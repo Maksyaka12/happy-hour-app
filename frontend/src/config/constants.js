1ff85e23b44fe8e3ae2462aee6922e870f691ee8
Maksyaka12 <maksim.soroka69@gmail.com>
2026-04-18 21:22:16 +0300

    feat: adjust bet options and reduce winning points reward

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -18,7 +18,7 @@ export const TICKET_UNIT = 0.1
 export const CHECKIN_AMOUNT = 0.0001
 export const CLOSE_BEFORE_MS = 3 * 60 * 1000
 
-export const BET_OPTS = [0.1, 0.5, 1, 5, 10, 30]
+export const BET_OPTS = [0.1, 0.5, 1, 3, 5, 10]
 
 export const STREAK_REWARDS = [
   { days: 3, pts: 5 },
