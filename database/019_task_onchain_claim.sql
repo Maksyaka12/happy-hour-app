eaf7709ced0945d2898b04318466903e446da54c
Maksyaka12 <maksim.soroka69@gmail.com>
2026-05-30 00:19:33 +0300

    security: untrack and ignore backend and database folders for public release

--- database/019_task_onchain_claim.sql
+++
@@ -1,7 +0,0 @@
--- database/019_task_onchain_claim.sql
--- Update claim_task_completion to require a tx_hash
-
--- First, add tx_hash column to task_completions if it doesn't exist
-ALTER TABLE task_completions ADD COLUMN IF NOT EXISTS tx_hash TEXT;
-
--- (The claim_task_completion function has been removed from here as it was rewritten in 020_activity_leaderboard.sql)
