c7a7c7b204adfb2edb96754b00e20d3df00ed24a
mksvibe <119020069+Maksyaka12@users.noreply.github.com>
2026-07-05 03:41:43 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,6 @@ import { WagmiProvider } from 'wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { config } from './config/wagmi'
 import App from './App'
-import './index.css'
 
 const queryClient = new QueryClient()
 
